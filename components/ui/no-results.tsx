0a2e1e1e9a7feb0663970e9d7cbefcf4faec9397
nahuelvera00 <nahuelvera3770@gmail.com>
2023-10-05 13:56:58 -0300

    first commit, added Product-list,product-card, actions, and more components

---
+++ components/ui/no-results.tsx
@@ -0,0 +1,11 @@
+interface NoResultsProsp {}
+
+const NoResults = () => {
+  return (
+    <div className="flex items-center justify-center h-full w-full text-neutral-500">
+      No results found.
+    </div>
+  );
+};
+
+export default NoResults;
